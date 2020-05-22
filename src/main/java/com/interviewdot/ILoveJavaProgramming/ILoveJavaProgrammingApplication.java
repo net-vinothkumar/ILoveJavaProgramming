7b8d8056b77d209ab29f44b9fc591b1f1648c6f6
vinothkumar <net.vinothkumar@gmail.com>
2020-05-22 12:02:07 +0200

    I love java programming

---
+++ src/main/java/com/interviewdot/ILoveJavaProgramming/ILoveJavaProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.interviewdot.ILoveJavaProgramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ILoveJavaProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ILoveJavaProgrammingApplication.class, args);
+	}
+
+}
